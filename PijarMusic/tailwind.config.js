1033d6148eebfddcfc75d598d4e998f4d12ec4d7
gedeastu <gede.astu@gmail.com>
2023-07-15 09:36:54 +0700

    up

--- PijarMusic/tailwind.config.js
+++ PijarMusic/tailwind.config.js
@@ -5,12 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
 
     },
   },
   plugins: [
     require('prettier-plugin-tailwindcss'),
-    
+
   ],
 }
