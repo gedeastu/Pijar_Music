28fe30eb42e8a20cbd7b0fa9627fe04913932395
gedeastu <gede.astu@gmail.com>
2023-07-19 12:23:13 +0700

    up

--- PijarMusic/src/pages/HomePage.jsx
+++ PijarMusic/src/pages/HomePage.jsx
@@ -1,12 +1,38 @@
 import './HomePage.css'
 import Header from '../components/Home/Header'
 import WhoWeAre from '../components/Home/WhoWeAre'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 export default function HomePage(){
     return(
         <>
         <div className='container bg-white mx-auto max-w-full flex flex-col h-max'>
         <Header/>
         <WhoWeAre/>
+        {/* <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={2}
+        navigation
+        pagination={
+            { clickable: true }
+        }
+        // scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        className='text-black w-full relative z-50 h-96'
+        >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper> */}
         </div>
         </>
     )
