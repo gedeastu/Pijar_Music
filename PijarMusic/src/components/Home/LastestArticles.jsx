90926452162b5e0f30479c542b016303193ca875
gedeastu <gede.astu@gmail.com>
2023-07-31 19:29:33 +0800

    up

--- PijarMusic/src/components/Home/LastestArticles.jsx
+++ PijarMusic/src/components/Home/LastestArticles.jsx
@@ -8,7 +8,7 @@ export default function LastestArticles(){
     useEffect(()=>{
         const fetchData = async () => {
          try{
-            const response = await axios.get(process.env.REACT_APP_API_URL);
+            const response = await axios.get('http://localhost:5000/LastestArticles/');
             setDataArticles(response.data)
          } catch (error){
             console.error("Error fetching data : ",error)
