18779e4f521e4fbe313a41183527976d253b211a
gedeastu <gede.astu@gmail.com>
2023-08-05 21:39:50 +0800

    up

--- PijarMusic/src/components/Home/Header.jsx
+++ PijarMusic/src/components/Home/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.css'
 import { useState } from 'react'
 import OurActivity from '../assets/OurActivity.svg'
+import * as AiIcons from "react-icons/ai";
 // import { Dropdown } from 'react-nested-dropdown';
 // import 'react-nested-dropdown/dist/styles.css';
 export default function Header(){
@@ -8,6 +9,17 @@ export default function Header(){
     const handleChange = () => {
         setIconSwap(!iconSwap);
     }
+
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const handleCloserDrawer = ()=> setDrawerOpen(false);
+    const handleOpenDrawer = () => setDrawerOpen(true);
+    const handleClick = () =>{
+        document.getElementById(btn_drawer).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+        handleCloserDrawer();
+    };
     // const itemsWhoWeAre = [
     //     {
     //       label: 'Option 1',
@@ -81,16 +93,18 @@ export default function Header(){
                 </button>
             </li>
             </ul>
-            <input id='drawer' type="checkbox" checked={iconSwap} onChange={handleChange} className='hidden peer'/>
-            <label htmlFor="drawer" className='relative z-[99] cursor-pointer md:hidden'>
+            <button><AiIcons.AiOutlineBars/></button>
+            {/* <input id='drawer' type="checkbox" checked={iconSwap} onChange={handleChange} className='hidden peer'/>
+            <label id='btn_drawer' onClick={handleOpenDrawer} htmlFor="drawer" className='relative z-[99] cursor-pointer md:hidden'>
             {iconSwap ? (<svg xmlns="http://www.w3.org/2000/svg" height="2em" className='fixed fill-[#FF6002]' viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>):(<svg xmlns="http://www.w3.org/2000/svg" height="2em" className='fill-white relative z-30' viewBox="0 0 512 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>)}
             </label>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" height="1.7em" className='fill-[#FF6002] z-40 top-5 fixed invisible peer-checked:visible' viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg> */}
+            <svg xmlns="http://www.w3.org/2000/svg" height="1.7em" className='fill-[#FF6002] z-40 top-5 fixed invisible peer-checked:visible' viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
             <div className='z-[70] fixed -top-96 left-0 w-screen h-96 bg-white peer-checked:shadow-2xl peer-checked:top-0 peer:transition-all duration-300'>  
             </div>
-            <div className="-z-30 fixed left-0 top-0 bg-gray-900 bg-opacity-30 h-screen w-screen duration-500 opacity-0 peer-checked:opacity-100 peer-checked:z-50 peer:transition-all"></div>
+            <div className="-z-30 fixed left-0 top-0 bg-gray-900 overflow-hidden bg-opacity-30 h-screen w-screen duration-500 opacity-0 peer-checked:opacity-100 peer-checked:z-50 peer:transition-all"></div> */}
             </div>
             </nav>
+            
             <div className='flex flex-col px-7 relative z-40 items-center gap-14 md:flex-row md:items-center md:px-28 md:justify-between'>
             <article className='text-white flex flex-col items-start justify-center h-[43rem] gap-9 font-outfit'>
                 <h1 className='font-semibold'>TRUSTED MUSIC COMPANY</h1>
